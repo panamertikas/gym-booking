1f45dfd232a1e7184a19c36c7524be2b42315c1d
panamertikas <mertikas91@gmail.com>
2026-05-11 20:19:01 +0300

    feat: add classCount

--- src/com/gymbooking/Main.java
+++ src/com/gymbooking/Main.java
@@ -6,23 +6,5 @@ public class Main {
     public static void main(String[] args) {
 
 
-        GymClass personal = new GymClass("Personal training", "George", 5);
-
-        GymClass semiPersonal = new GymClass("Semi Personal", "Maria", 12);
-
-
-        Member m1 = new Member("Panos", "Mertikas", "panos@mail.com", -5);
-        System.out.println(m1);
-
-        Member m2 = new Member();
-        m2.setAge(-10);
-
-        GymClass yoga = new GymClass("Yoga", "Maria", 2);
-        yoga.addMember();
-        yoga.addMember();
-        yoga.addMember(); // γεμάτο
-        yoga.removeMember();
-        yoga.removeMember();
-        yoga.removeMember(); // άδειο
     }
 }
