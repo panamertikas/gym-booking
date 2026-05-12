81fdcb47f76418f65232a693a7bf2e1619c83e07
panamertikas <mertikas91@gmail.com>
2026-05-12 15:17:33 +0300

    feat: Add polymorphism example with printPersonInfo method

--- src/com/gymbooking/Main.java
+++ src/com/gymbooking/Main.java
@@ -1,10 +1,25 @@
 package com.gymbooking;
-import com.gymbooking.model.GymClass;
-import com.gymbooking.model.Member;
+import com.gymbooking.exceptions.GymClassEmptyException;
+import com.gymbooking.exceptions.GymClassFullException;
+import com.gymbooking.model.*;
 
 public class Main {
+
+    public static void printPersonInfo(Person person) {
+        System.out.println(person);
+    }
+
     public static void main(String[] args) {
 
+        Member m = Member.of("Panos", "Mertikas", "panos@mail.com", 25);
+        Trainer t = Trainer.of("Maria", "Papadopoulou", 35, "Yoga");
 
+        printPersonInfo(m);
+        printPersonInfo(t);
     }
 }
+
+
+
+
+
