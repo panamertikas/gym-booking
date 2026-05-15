d31d2d36199cfdf00ec733bb401d11ae847cc1e5
panamertikas <mertikas91@gmail.com>
2026-05-15 19:25:32 +0300

    feat: Add DAO and Service layers with MemberDao, GymClassDao, BookingDao and corresponding services

--- src/com/gymbooking/Main.java
+++ src/com/gymbooking/Main.java
@@ -4,25 +4,26 @@ import com.gymbooking.exceptions.GymClassEmptyException;
 import com.gymbooking.exceptions.GymClassFullException;
 import com.gymbooking.interfaces.IPrintable;
 import com.gymbooking.model.*;
+import com.gymbooking.service.GymClassService;
+import com.gymbooking.service.MemberService;
 
 public class Main {
 
     public static void main(String[] args) {
-        GymApp gym = new GymApp();
+        MemberService memberService = new MemberService();
+        GymClassService gymClassService = new GymClassService();
 
         Member m1 = Member.of("Panos", "Mertikas", "panos@mail.com", 25);
         Member m2 = Member.of("Maria", "Papadopoulou", "maria@mail.com", 30);
 
         GymClass yoga = GymClass.of("Yoga", "Maria", 10);
 
-        gym.addMember(m1);
-        gym.addMember(m2);
-        gym.addClass(yoga);
+        memberService.addMember(m1);
+        memberService.addMember(m2);
+        gymClassService.addGymClass(yoga);
 
-        System.out.println("Members: " + gym.getMembers().size());
-        System.out.println("Classes: " + gym.getGymClasses().size());
-
-        gym.printAllMembers();
+        System.out.println("Members: " + memberService.getAllMembers().size());
+        System.out.println("Classes: " + gymClassService.getAllGymClasses().size());
     }
 }
 
